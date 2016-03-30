eaf85eeb2d1a589674a9e0670f5eb0af3fa6c897
pc007007 <pc19900804@gmail.com>
2016-03-30 19:26:19 -0400

    #change postgresql setting

--- src/main/resources/static/blogApp/blogService.js
+++ src/main/resources/static/blogApp/blogService.js
@@ -2,7 +2,7 @@
  * Created by pc on 3/25/16.
  */
 angular.module('blogApp.services', []).factory('Post', function($resource) {
-    return $resource('http://localhost:8080/api/posts/:id', { id: '@_id' }, {
+    return $resource('/api/posts/:id', { id: '@_id' }, {
         update: {
             method: 'PUT'
         }
