194ecc8efb9e56a432fc754ed11466097dfc7c12
pc007007 <pc19900804@gmail.com>
2016-04-05 19:16:23 -0400

    #change ddl 2 update

--- src/main/java/com/chengp/repository/TagRepository.java
+++ src/main/java/com/chengp/repository/TagRepository.java
@@ -1,10 +1,9 @@
 package com.chengp.repository;
 
-import com.chengp.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by pc on 4/4/16.
  */
-public interface TagRepository extends JpaRepository<Tag,Integer>{
-}
+/*public interface TagRepository extends JpaRepository<Tag,Integer>{
+}*/
